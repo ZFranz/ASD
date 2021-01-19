9b887809aba3ccaa23ed5b632b7224c86d33278d
ZFranz <zhou.francesco98@gmail.com>
2021-01-19 17:18:03 +0100

    Add files via upload

---
+++ ASD/prima/periodoNaive.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+using namespace std;
+
+int periodoNaive(string s) {
+	int n = s.length;
+	for (int p = 1; p <= n; p++) {
+		string s1 = s.substring(0, n-p);
+	}
+}
+
+int main() {
+	
+	
+	return 0;
+}
